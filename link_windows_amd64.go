baa9cc91c5d4bf5fc7423005d13d5a9bc33798c3
mohamedSulaimanAlmarzooqi <uae566340007@hotmail.com>
2022-06-11 10:51:25 +0400

    Updates the synthizer build to latest.

--- link_windows_amd64.go
+++ link_windows_amd64.go
@@ -23,7 +23,7 @@
 package synthizer
 
 /*
-#cgo windows LDFLAGS: -Llibs/windows ${SRCDIR}/libs/windows/libsynthizer.a -static-libgcc -static-libstdc++  -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic -lkernel32 -luser32 -lole32 -lwinmm -ldsound -ldxguid
+#cgo windows LDFLAGS: -Llibs/windows ${SRCDIR}/libs/windows/libsynthizer.a -static-libgcc -static-libstdc++  -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic -lkernel32 -luser32 -lole32 -lwinmm -ldsound
 #cgo windows CPPFLAGS: -O1 -I${SRCDIR}/libs/include
 #cgo windows CFLAGS: -O1 -I${SRCDIR}/libs/include
 */
